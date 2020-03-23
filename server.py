3e008525a990820bd81335047e6600d336c2fd08
Michael Leue <domus@posteo.de>
2020-03-23 08:26:56 +0100

    add a socket loop for continuous data exchange until an empty message is received on the server

--- server.py
+++ server.py
@@ -9,10 +9,14 @@ def serve_forever(host, port):
     while True:
         client_socket, address = server_socket.accept()
         print(f"Socket established with {address}.")
-        data = client_socket.recv(1024)
-        print(f"Received {data}")
-        client_socket.send(data)
+        while True:
+            data = client_socket.recv(1024)
+            print(f"Received {data}")
+            if data == b"":
+                break
+            client_socket.send(data)
         client_socket.close()
+        print(f"Socket with {address} closed.")
 
 
 if __name__ == "__main__":
