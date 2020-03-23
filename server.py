a01f487492f5686fe76696f155de96e3ba89f5ad
Michael Leue <domus@posteo.de>
2020-03-23 08:28:09 +0100

    add capability for handling several client sockets in parallel through threading

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
+from typing import Tuple
 import socket
+import threading
 
 
-def serve_forever(host, port):
+def handle_socket(client_socket, address: Tuple[str, int]):
+    while True:
+        data = client_socket.recv(1024)
+        print(f"Received {data}")
+        if data == b"":
+            break
+        client_socket.send(data)
+    client_socket.close()
+    print(f"Socket with {address} closed.")
+
+
+def serve_forever(host: str, port: int):
     server_socket = socket.socket()
     server_socket.bind((host, port))
     server_socket.listen(1)
@@ -9,14 +22,10 @@ def serve_forever(host, port):
     while True:
         client_socket, address = server_socket.accept()
         print(f"Socket established with {address}.")
-        while True:
-            data = client_socket.recv(1024)
-            print(f"Received {data}")
-            if data == b"":
-                break
-            client_socket.send(data)
-        client_socket.close()
-        print(f"Socket with {address} closed.")
+        t = threading.Thread(
+            target=handle_socket, args=(client_socket, address)
+        )
+        t.start()
 
 
 if __name__ == "__main__":
