5e2ae5208c24354ca6997e04faf281e84f831bc7
Michael Leue <domus@posteo.de>
2020-04-01 17:02:24 +0200

    a first iteration of the WSGI application framework with a simple Application that can register GET or POST endpoints via decorators

--- run.py
+++ run.py
@@ -1,19 +1,18 @@
-from flask import Flask, request
 from wsgi.server import WSGIServer
+from wsgi.application import WSGIApplication
+
+app = WSGIApplication()
 
 
-app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
+@app.get("/")
 def root():
     print("Called root endpoint.")
     return "hello from /"
 
 
-@app.route("/create", methods=["POST"])
+@app.post("/create")
 def create():
-    print(f"Called create endpoint with data {request.data}.")
+    print(f"Called /create endpoint.")
     return "hello from /create"
 
 
